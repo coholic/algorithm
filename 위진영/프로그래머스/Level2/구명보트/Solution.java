13348da30410347347d28d7bd1768781150dc29b
weejinyoung <a8118199@gmail.com>
2023-08-28 17:57:25 +0900

    3주차 프로그래머스

---
+++ 위진영/프로그래머스/Level2/구명보트/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int boat = 0;
+
+        Arrays.sort(people);
+        List<Integer> peopleList = new ArrayList<>();
+
+        for (int i = people.length - 1; i >= 0; i--) {
+            peopleList.add(people[i]);
+        }
+
+
+        Deque<Integer> pepleDeque = new ArrayDeque<>(peopleList);
+
+        while (!pepleDeque.isEmpty()) {
+            if (pepleDeque.peekFirst() + pepleDeque.peekLast() <= limit) {
+                pepleDeque.pollFirst();
+                pepleDeque.pollLast();
+            } else {
+                pepleDeque.pollFirst();
+            }
+            boat++;
+        }
+
+        return boat;
+
+    }
+
+
+}
