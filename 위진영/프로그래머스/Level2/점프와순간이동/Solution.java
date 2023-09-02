6e81b02658ca2ff716847a572c339189c421aad4
weejinyoung <a8118199@gmail.com>
2023-09-02 17:53:02 +0900

    프로그래머스 5주차

---
+++ 위진영/프로그래머스/Level2/점프와순간이동/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int solution(int n) {
+        int consumption = 1;
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+                consumption++;
+            }
+        }
+
+        return consumption;
+
+    }
+}
