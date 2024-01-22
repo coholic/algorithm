5f1c5abc037f7be59dddb44ac050f2aabfce64bc
Minuooooo <mmw7741@gmail.com>
2024-01-22 19:57:32 +0900

    feat: 가장 큰 정사각형 찾기

---
+++ 문민우/프로그래머스/level2/가장큰정사각형찾기/Solution.java
@@ -0,0 +1,29 @@
+package level2.가장큰정사각형찾기;
+
+public class Solution {
+    public int solution(int[][] board) {
+
+        int answer = 0;
+        int row = board.length;
+        int col = board[0].length;
+
+        if (row < 2 || col < 2)
+            return 1;
+
+        for (int i = 1; i < row; i++) {
+
+            for (int j = 1; j < col; j++) {
+
+                if (board[i][j] != 0) {
+
+                    board[i][j] = Math.min(board[i - 1][j - 1],
+                            Math.min(board[i - 1][j], board[i][j - 1])) + 1;
+                }
+
+                answer = Math.max(answer, board[i][j]);
+            }
+        }
+
+        return answer * answer;
+    }
+}
